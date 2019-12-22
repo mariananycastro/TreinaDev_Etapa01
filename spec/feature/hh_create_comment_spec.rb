9c40c8194b56ca3d07ed1114a88aa3cc61e398a5
Mariana Naomi Yamada Castro <mariananycastro@gmail.com>
2019-12-22 10:10:22 -0300

    more tests with subscriptions

--- spec/feature/hh_create_comment_spec.rb
+++ spec/feature/hh_create_comment_spec.rb
@@ -92,4 +92,69 @@ require 'rails_helper'
  
             expect(page).to have_link 'Adicionar Comentário'
         end
+
+        scenario 'job seeker subscribes for more than one job opportunity' do
+
+            job_seeker = JobSeeker.create!(email: 'js@test.com', password: '123456')
+            profile = Profile.create!(name: 'Pedro', nick_name: 'Pedrinho', day_of_birth: '1986-02-02', 
+                                        education_level: 'Graduado',
+                                        description: 'UEM', experience: 'Ja trabalhei com TI', document: '521.730.870-29',
+                                        job_seeker: job_seeker)
+            headhunter = Headhunter.create!(email: 'hh@test.com', password: '123456')        
+            job_opportunity = JobOpportunity.create!(headhunter: headhunter, name: 'Programador Ruby', description: 'Vaga para programador Ruby',
+                                                       habilities: 'Saber programar', salary_range: 5000, 
+                                                       opportunity_level: 'Pleno', end_date_opportunity: '02/02/2020',
+                                                       region: 'São Paulo')
+            subscription = Subscription.create!(job_seeker:job_seeker, job_opportunity:job_opportunity)
+            job_opportunity2 = JobOpportunity.create!(headhunter: headhunter, name: 'Programador Ruby2', description: 'Vaga para programador Ruby',
+                                                        habilities: 'Saber programar', salary_range: 5000, 
+                                                        opportunity_level: 'Pleno', end_date_opportunity: '02/02/2020',
+                                                        region: 'São Paulo')
+            subscription = Subscription.create!(job_seeker:job_seeker, job_opportunity:job_opportunity2)
+            
+    
+            login_as(headhunter, scope: :headhunter)
+            visit root_path
+            click_on 'Vagas Cadastradas'
+            click_on "#{job_opportunity.name} #{job_opportunity.opportunity_level}"\
+                    " #{job_opportunity.end_date_opportunity} #{job_opportunity.region}"
+            click_on "#{profile.name} #{profile.document} #{profile.education_level}"
+            
+            expect(page).to have_content ('Programador Ruby Pleno 2020-02-02 São Paulo')
+            expect(page).to have_content ('Programador Ruby2 Pleno 2020-02-02 São Paulo')
+
+        end
+
+        scenario 'job seeker subscribes two job opportunities from different headhunter' do
+
+            job_seeker = JobSeeker.create!(email: 'js@test.com', password: '123456')
+            profile = Profile.create!(name: 'Pedro', nick_name: 'Pedrinho', day_of_birth: '1986-02-02', 
+                                        education_level: 'Graduado',
+                                        description: 'UEM', experience: 'Ja trabalhei com TI', document: '521.730.870-29',
+                                        job_seeker: job_seeker)
+            headhunter = Headhunter.create!(email: 'hh@test.com', password: '123456')
+            headhunter2 = Headhunter.create!(email: 'hh2@test.com', password: '123456')        
+            job_opportunity = JobOpportunity.create!(headhunter: headhunter, name: 'Programador Ruby', description: 'Vaga para programador Ruby',
+                                                       habilities: 'Saber programar', salary_range: 5000, 
+                                                       opportunity_level: 'Pleno', end_date_opportunity: '02/02/2020',
+                                                       region: 'São Paulo')
+            subscription = Subscription.create!(job_seeker:job_seeker, job_opportunity:job_opportunity)
+            job_opportunity2 = JobOpportunity.create!(headhunter: headhunter2, name: 'Programador Ruby2', description: 'Vaga para programador Ruby',
+                                                        habilities: 'Saber programar', salary_range: 5000, 
+                                                        opportunity_level: 'Pleno', end_date_opportunity: '02/02/2020',
+                                                        region: 'São Paulo')
+            subscription = Subscription.create!(job_seeker:job_seeker, job_opportunity:job_opportunity2)
+            
+    
+            login_as(headhunter, scope: :headhunter)
+            visit root_path
+            click_on 'Vagas Cadastradas'
+            click_on "#{job_opportunity.name} #{job_opportunity.opportunity_level}"\
+                    " #{job_opportunity.end_date_opportunity} #{job_opportunity.region}"
+            click_on "#{profile.name} #{profile.document} #{profile.education_level}"
+            
+            expect(page).to have_content ('Programador Ruby Pleno 2020-02-02 São Paulo')
+            expect(page).not_to have_content ('Programador Ruby2 Pleno 2020-02-02 São Paulo')
+
+        end
     end
--- spec/feature/hh_view_jj_subscribed_to_job_opp_spec.rb
+++ spec/feature/hh_view_jj_subscribed_to_job_opp_spec.rb
@@ -48,4 +48,37 @@ feature "headhunter views subscriptions of job seeker and job seeker's profile" 
         expect(page).to have_content 'Não há inscrições para esta vaga'
         
     end
+
+    scenario 'more than one subscription' do
+        
+        job_seeker = JobSeeker.create!(email: 'js@test.com', password: '123456')
+        profile = Profile.create!(name: 'Pedro', nick_name: 'Pedrinho', day_of_birth: '1986-02-02', 
+                                    education_level: 'Graduado',
+                                    description: 'UEM', experience: 'Ja trabalhei com TI', document: '521.730.870-29',
+                                    job_seeker: job_seeker)
+        job_seeker2 = JobSeeker.create!(email: 'js2@test.com', password: '123456')
+        profile2 = Profile.create!(name: 'Ana', nick_name: 'Aninha', day_of_birth: '1986-03-02', 
+                                    education_level: 'Graduada',
+                                    description: 'USP', experience: 'Ja trabalhei muito com TI', document: '911.386.250-27',
+                                    job_seeker: job_seeker2)
+        headhunter = Headhunter.create!(email: 'hh@test.com', password: '123456')        
+        job_opportunity = JobOpportunity.create!(headhunter: headhunter, name: 'Programador Ruby', 
+                                                   description: 'Vaga para programador Ruby',
+                                                   habilities: 'Saber programar', salary_range: 5000, 
+                                                   opportunity_level: 'Pleno', end_date_opportunity: '02/02/2020',
+                                                   region: 'Sâo Paulo')
+        subscription = Subscription.create!(job_seeker:job_seeker, job_opportunity:job_opportunity)        
+        subscription2 = Subscription.create!(job_seeker:job_seeker2, job_opportunity:job_opportunity)        
+
+        login_as(headhunter, scope: :headhunter)
+        visit root_path
+        click_on 'Vagas Cadastradas'
+        click_on "#{job_opportunity.name} #{job_opportunity.opportunity_level}"\
+                " #{job_opportunity.end_date_opportunity} #{job_opportunity.region}"
+            
+        expect(page).to have_content "#{profile.name} #{profile.document} #{profile.education_level}"
+        expect(page).to have_content "#{profile2.name} #{profile2.document} #{profile2.education_level}"
+        
+    end
+
 end
