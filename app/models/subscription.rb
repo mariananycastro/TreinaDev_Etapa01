3f8630000ba8ed0b6cd5ace65cc99a254328a2f7
Mariana Naomi Yamada Castro <mariananycastro@gmail.com>
2019-12-24 16:57:40 -0300

    job seeker cant subscribe if date end of opportunity less than today

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,9 +1,17 @@
 class Subscription < ApplicationRecord
+    validate :end_date_subscription
+
     belongs_to :job_seeker
     belongs_to :job_opportunity
     belongs_to :hh_answer,  polymorphic: true, optional: true
 
     has_one :subscription_comment
     enum status: {not_set: 0, invited: 5, rejected: 10}
-    
+
+    def end_date_subscription
+        end_date = job_opportunity.end_date_opportunity
+        if end_date <= Date.current
+            errors.add(:end_date_subscription, 'já encerrada')    
+          end  
+    end
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
-RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  feature 'end date opportunity must be greater than yesterday to job seeker subscribe' do
+    it '= today'do
+      headhunter = Headhunter.create!(email: 'hh@test.com', password: '123456')        
+      job_opportunity = JobOpportunity.new(headhunter: headhunter, name: 'Programador Ruby', description: 'Vaga para programador Ruby',
+                                                 habilities: 'Saber programar', salary_range: 5000, 
+                                                 opportunity_level: 'Pleno', end_date_opportunity: Date.current,
+                                                 region: 'Sâo Paulo')
+      job_seeker = JobSeeker.create!(email: 'js@test.com', password: '123456')
+      profile = Profile.create!(name: 'Pedro', nick_name: 'Pedrinho', day_of_birth: '1986-02-02', 
+                                    education_level: 'Graduado',
+                                    description: 'UEM', experience: 'Ja trabalhei com TI', document: '521.730.870-29',
+                                    job_seeker: job_seeker, avatar: fixture_file_upload('avatar.JPG'))
+      subscription = Subscription.new(job_seeker:job_seeker, job_opportunity:job_opportunity)  
+      
+      expect(subscription.valid?).to eq false
+      expect(subscription.errors.full_messages).to include('End date subscription já encerrada')
+    end
+  
+    it '>  today' do
+      headhunter = Headhunter.create!(email: 'hh@test.com', password: '123456')        
+      job_opportunity = JobOpportunity.new(headhunter: headhunter, name: 'Programador Ruby', description: 'Vaga para programador Ruby',
+                                                 habilities: 'Saber programar', salary_range: 5000, 
+                                                 opportunity_level: 'Pleno', end_date_opportunity: 2.days.from_now,
+                                                 region: 'Sâo Paulo')
+      job_seeker = JobSeeker.create!(email: 'js@test.com', password: '123456')
+      profile = Profile.create!(name: 'Pedro', nick_name: 'Pedrinho', day_of_birth: '1986-02-02', 
+                                    education_level: 'Graduado',
+                                    description: 'UEM', experience: 'Ja trabalhei com TI', document: '521.730.870-29',
+                                    job_seeker: job_seeker, avatar: fixture_file_upload('avatar.JPG'))
+      subscription = Subscription.new(job_seeker:job_seeker, job_opportunity:job_opportunity)  
+      
+      subscription.valid?
+      
+      expect(subscription.errors).to be_empty
+    end
+  
+    it '<  today' do
+      headhunter = Headhunter.create!(email: 'hh@test.com', password: '123456')        
+      job_opportunity = JobOpportunity.new(headhunter: headhunter, name: 'Programador Ruby', description: 'Vaga para programador Ruby',
+                                                 habilities: 'Saber programar', salary_range: 5000, 
+                                                 opportunity_level: 'Pleno', end_date_opportunity: 2.days.ago,
+                                                 region: 'Sâo Paulo')
+      job_seeker = JobSeeker.create!(email: 'js@test.com', password: '123456')
+      profile = Profile.create!(name: 'Pedro', nick_name: 'Pedrinho', day_of_birth: '1986-02-02', 
+                                    education_level: 'Graduado',
+                                    description: 'UEM', experience: 'Ja trabalhei com TI', document: '521.730.870-29',
+                                    job_seeker: job_seeker, avatar: fixture_file_upload('avatar.JPG'))
+      subscription = Subscription.new(job_seeker:job_seeker, job_opportunity:job_opportunity)  
+      
+      
+      expect(subscription.valid?).to eq false
+      expect(subscription.errors.full_messages).to include('End date subscription já encerrada')
+    end
+  end
