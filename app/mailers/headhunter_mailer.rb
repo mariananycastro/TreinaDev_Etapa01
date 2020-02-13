11e6867711ada89d784d4804fb8f06e225008ef4
Mariana Naomi Yamada Castro <mariananycastro@gmail.com>
2020-02-13 16:23:12 -0300

    email to hh about js subscription done

--- app/mailers/headhunter_mailer.rb
+++ app/mailers/headhunter_mailer.rb
@@ -1,10 +1,9 @@
 class HeadhunterMailer < ApplicationMailer
     default from: 'teste@teste.com'
 
-    def welcome_email(headhunter, job_opportunity, job_seeker)
+    def welcome_email(headhunter, subscription)
         @headhunter = headhunter
+        @subscription = subscription
         mail(to: @headhunter.email, subject: 'teste')
-        @job_opportunity = job_opportunity
-        @job_seeker = job_seeker
     end
 end
--- spec/mailers/headhunter_mailer_spec.rb
+++ spec/mailers/headhunter_mailer_spec.rb
@@ -10,16 +10,15 @@ RSpec.describe HeadhunterMailer, type: :mailer do
       subscription = create(:subscription, job_seeker_id: job_seeker.id, job_opportunity_id: job_opportunity.id)
 
       ActiveJob::Base.queue_adapter = :test
-      email = HeadhunterMailer.welcome_email(headhunter, job_opportunity, job_seeker)
-      
-      expect { email.deliver_later }.to have_enqueued_job.on_queue('mailers')
-      #byebug
+      email = HeadhunterMailer.welcome_email(headhunter, subscription)
+
+      expect{ email.deliver_later }.to have_enqueued_job.on_queue('mailers')
       expect(email.subject).to eq 'teste'
       expect(email.body).to have_content 'email de teste'
       expect(email.body).to have_content (headhunter.email)
       expect(email.body).to have_content (job_opportunity.name)
       expect(email.body).to have_content (job_seeker.email)
-      expect(HeadhunterMailer.deliveries.count).to eq 1
+      
     end
 
     # since we have config.action_mailer.delivery_method  set to :test in our :test.rb, all 'sent' emails are gathered into the ActionMailer::Base.deliveries array.
@@ -31,9 +30,10 @@ RSpec.describe HeadhunterMailer, type: :mailer do
 
       expect {
         perform_enqueued_jobs do
-            HeadhunterMailer.welcome_email(headhunter).deliver_later
+            HeadhunterMailer.welcome_email(headhunter, subscription).deliver_later
         end
       }.to change { ActionMailer::Base.deliveries.size }.by(1)
+      expect(HeadhunterMailer.deliveries.count).to eq 1
     end
 
     it 'welcome_email is sent to the right user' do
@@ -41,8 +41,9 @@ RSpec.describe HeadhunterMailer, type: :mailer do
       job_seeker = create(:job_seeker)
       job_opportunity = create(:job_opportunity, headhunter_id: headhunter.id)
       subscription = create(:subscription, job_seeker_id: job_seeker.id, job_opportunity_id: job_opportunity.id)
+
       perform_enqueued_jobs do
-          HeadhunterMailer.welcome_email(headhunter).deliver_later
+          HeadhunterMailer.welcome_email(headhunter, subscription).deliver_later
       end
   
       mail = ActionMailer::Base.deliveries.last
@@ -51,8 +52,3 @@ RSpec.describe HeadhunterMailer, type: :mailer do
     end
   end
 end
-
-
-  
-
-    #to have_enqueued_job(ActionMailer::DeliveryJob).with('ReportMailer', 'update_mail', 'deliver_now', user.id)
